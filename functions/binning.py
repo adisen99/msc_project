12e5fdaafeef609039e62cbe59ac7203ff255d20
adisen99 <adityasenguptasai99@gmail.com>
2021-09-25 20:26:59 +0530

    added analysis01 and modified functions

--- functions/binning.py
+++ functions/binning.py
@@ -9,22 +9,63 @@ def equalObs(x, nbin):
     nlen = len(x)
     return np.interp(np.linspace(0, nlen, nbin + 1),
                      np.arange(nlen),
-                     np.sort(x))
+                     np.sort(x, axis= None))
 
 #-----------------------------------------------------#
 
 # Using function
 
 # get binned data
-def get_binned(ds, percentile_val, bins = None, bin_nr = 12):
-    t2m = ds.t2m
+def get_binned(ds, percentile_val = 0.99, var = "t2m", bins = None, bin_nr = 12):
     if bins == None:
         #create histogram with equal-frequency bins
-        # n, bins, patches = plt.hist(t2m, equalObs(t2m, bin_nr))
+        # n, bins, patches = plt.hist(ds[var], equalObs(ds[var], bin_nr))
         # plt.show()
-        bins = equalObs(t2m, bin_nr)
+        bins = equalObs(ds[var], bin_nr)
     else:
         bins = np.array(bins)
 
-    binned_ds = ds.groupby_bins('t2m', bins).quantile(percentile_val, interpolation = 'midpoint')
+    binned_ds = ds.groupby_bins(ds[var], bins).quantile(percentile_val, interpolation = 'midpoint')
     return binned_ds
+
+def get_binned_3d(ds, min = 1, percentile_val = 0.99, var = "t2m", var_bin = "t2m_bins", bin_nr = 12):
+
+    precip = ds.precipitationCal
+    temp = ds[var]
+
+    precip = xr.where(precip < min, np.nan, precip)
+    temp = temp.where(precip != np.nan)
+    
+    ds_comb = xr.merge([precip, temp]).chunk(dict(time=-1))
+
+    precip = ds_comb.precipitationCal.to_numpy()
+    temp = ds_comb[var].to_numpy()
+
+    bins = np.apply_along_axis(equalObs, 0, temp, bin_nr)
+
+    binned_ds = np.zeros((len(bins[0]), len(bins[0][0])))
+    
+    for lat in range(len(bins[0])):
+        for lon in range(len(bins[0][0])):
+            y = ds.isel(lat = lat, lon = lon).groupby_bins(ds[var].isel(lat = lat, lon = lon), bins[:, lat, lon]).quantile(percentile_val, interpolation='midpoint')
+            
+            bin_array = y.coords[var_bin].to_numpy()
+        
+            bin_mids = []
+
+            for i in range(0, len(bin_array)):
+                bin_mid = (bin_array[i].left + bin_array[i].right)*0.5
+                bin_mids.append(bin_mid)
+
+            mids = np.array(bin_mids)
+        
+            if np.isnan(np.sum(y.precipitationCal.to_numpy())):
+                slope = np.nan
+            else:
+                slope = np.polyfit(mids, y.precipitationCal.to_numpy(), 1)[0]
+            
+            binned_ds[lat, lon] = binned_ds[lat, lon] + slope
+
+    ccscale = xr.DataArray(binned_ds, dims=("lat", "lon"), coords={"lat": ds.coords['lat'], "lon": ds.coords['lon']}, attrs=dict(description="C-C scale", units="degC$^{-1}$"))
+    
+    return ccscale
--- functions/ccplot.py
+++ functions/ccplot.py
@@ -4,8 +4,8 @@ import xarray as xr
 
 # Preparation for the ideal C-C scaling background plots
 # TODO - implement another background plot of 2*CC scaling or 1.5*CC scaling
-def get_ideal_data(t2m_da, p11_inital_val, p12_initial_value):
-    temparr = np.sort(t2m_da)
+def get_ideal_data(da, p11_inital_val, p12_initial_value):
+    temparr = np.sort(da)
     p11 = [p11_inital_val]
     p12 = [p12_initial_value]
 
@@ -21,20 +21,16 @@ def get_ideal_data(t2m_da, p11_inital_val, p12_initial_value):
     return temparr, preciparr1, preciparr2
 
 # The main plot function
-def plot(ds, binned_ds, temparr, preciparr1, preciparr2, fit = True, **kwargs):
+def plot(ds, binned_ds, bin_var, temparr, preciparr1, preciparr2, fit = True, **kwargs):
     # get the mid points of the temperature bins
     bin_mids = []
-    bin_array = binned_ds.coords['t2m_bins'].to_numpy()
+    bin_array = binned_ds.coords[bin_var].to_numpy()
 
     for i in range(0, len(bin_array)):
         bin_mid = (bin_array[i].left + bin_array[i].right) * 0.5
         bin_mids.append(bin_mid)
 
     mids = np.array(bin_mids)
-
-    # set the data arrays to be used.
-    t2m = ds.t2m
-    precip = ds.precipitationCal
 
     # Make the figure
     if fit == True:
@@ -46,7 +42,7 @@ def plot(ds, binned_ds, temparr, preciparr1, preciparr2, fit = True, **kwargs):
         plt.semilogy(temparr, preciparr1, 'k--', alpha = 0.3)
         plt.semilogy(temparr, preciparr2, 'k--', alpha = 0.3)
         plt.semilogy(mids, slope*mids + intercept, color = 'k', ls = '-', alpha = 0.8, label = f'C-C scale = {np.round(slope, 3)}')
-        plt.xlim(t2m.min(),t2m.max())
+        plt.xlim(mids.min() - 1, mids.max() + 1)
         plt.yticks([1, 10, 100])
         plt.legend(frameon = False)
 
@@ -58,6 +54,6 @@ def plot(ds, binned_ds, temparr, preciparr1, preciparr2, fit = True, **kwargs):
         # binned_ds.precipitationCal.plot(**kwargs)
         plt.semilogy(temparr, preciparr1, 'k--', alpha = 0.3)
         plt.semilogy(temparr, preciparr2, 'k--', alpha = 0.3)
-        plt.xlim(t2m.min(),t2m.max())
+        plt.xlim(mids.min() - 1, mids.max() + 1)
         plt.yticks([1, 10, 100])
         plt.legend(frameon = False)
