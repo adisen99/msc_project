6c8ce43f3562065ff007a8dcad24768751e6f758
adisen99 <adityasenguptasai99@gmail.com>
2021-09-30 19:35:07 +0530

    lab: changed gpmdown file and added refactoring sources to read

--- README.md
+++ README.md
@@ -3,13 +3,17 @@ Code, functions and notebooks used for my Masters Project/Thesis.
 
 ### Some usefule  resources for data binning technique -
 
+- http://xarray.pydata.org/en/stable/user-guide/reshaping.html?highlight=stack#stack-and-unstack [READ THIS for refactoring]
+- https://groups.google.com/g/xarray/c/fz7HHgpgwk0/m/h0umDBIHAAAJ [related to the link above]
+
 - https://www.saedsayad.com/unsupervised_binning.htm
 - https://www.statology.org/equal-frequency-binning-python/
 - https://stackoverflow.com/questions/56485160/xarray-equivalent-of-pandas-qcut-function
 
 ### TODO -
 
+- [ ] Refactor the binning for 3d plotting
 - [x] Bin temperature data and take 99th and 50th percentile of precipitation.
-- [ ] Bin the temperature data such that the precipitation has same frequency in each bin.
-- [ ] Get the slope of the final plot.
-- [ ] Find a way to present this for a lat-lon plot.
+- [x] Bin the temperature data such that the precipitation has same frequency in each bin.
+- [x] Get the slope of the final plot.
+- [x] Find a way to present this for a lat-lon plot.
--- util/gpmdown.py
+++ util/gpmdown.py
@@ -1,29 +1,30 @@
 from typing import Sequence
 import requests
 from multiprocessing.pool import ThreadPool
-
 from requests.sessions import Session, session
+from os.path import exists
 
 username = "adisen99"
 password = "Sai@baba99"
 
 def download_url(url):
-    print("downloading: ", url)
     file_name_start_pos = url.rfind("/") + 1
-    file_name_end_pos = url.rfind("?") - 1
+    file_name_end_pos = url.rfind("?")
     filename = url[file_name_start_pos:file_name_end_pos]
 
-    with requests.Session() as session:
-        session.auth = (username, password)
-        r1 = session.request('get', url)
-        r = session.get(r1.url, auth=(username, password))
-        if r.ok:
-            with open(filename, 'wb') as f:
-                for data in r:
-                    f.write(data)
+    if not exists(filename):
+        print("downloading: ", url)
+        with requests.Session() as session:
+            session.auth = (username, password)
+            r1 = session.request('get', url)
+            r = session.get(r1.url, auth=(username, password))
+            if r.ok:
+                with open(filename, 'wb') as f:
+                    for data in r:
+                        f.write(data)
 
 # Get the urls from the txt file as a list using readline
-file1 = open('myfile.txt', 'r')
+file1 = open('links.txt', 'r')
 Lines = file1.readlines()
 
 urls = []
@@ -34,6 +35,6 @@ for line in Lines:
     urls.append(line.strip())
 
 # Run 5 multiple threads. Each call will take the next element in urls list
-results = ThreadPool(5).imap_unordered(download_url, urls)
+results = ThreadPool(6).imap_unordered(download_url, urls)
 for r in results:
-    print(r)
+    print("Completed" + r)
