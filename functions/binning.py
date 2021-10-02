a2efd77403f7f31953f2b3ccc994abcb43dc7225
adisen99 <adityasenguptasai99@gmail.com>
2021-10-02 19:51:11 +0530

    lab: refactored binning function and download scripts

--- functions/binning.py
+++ functions/binning.py
@@ -11,6 +11,21 @@ def equalObs(x, nbin):
                      np.arange(nlen),
                      np.sort(x, axis= None))
 
+def get_slope(x, y):
+    bin_mid = []
+    for i in range(0, len(x)):
+        bin_mid = (x[i].left + x[i].right)*0.5
+        x.append(bin_mid)
+
+    mids = np.array(x)
+
+    if np.isnan(np.sum(y)):
+        slope = np.nan
+    else:
+        slope = np.polyfit(mids, y, 1)[0]
+
+    return slope
+
 #-----------------------------------------------------#
 
 # Using function
@@ -63,35 +78,27 @@ def get_binned_3d_old(ds, percentile_val = 0.99, var = "t2m", var_bin = "t2m_bin
     return ccscale
 
 def get_binned_3d(ds, percentile_val = 0.99, var = "t2m", var_bin = "t2m_bins", bin_nr = 12):
-
-    precip = ds.precipitationCal.to_numpy()
+    # get the data arrays
     temp = ds[var].to_numpy()
+    # precip = ds.precipitationCal.to_numpy()
 
+    # get the bins and flatten them
     bins = np.apply_along_axis(equalObs, 0, temp, bin_nr)
+    bins = bins.reshape(bin_nr + 1, len(bins[0] * len(bins[0][0])))
 
-    binned_ds = np.zeros((len(bins[0]), len(bins[0][0])))
+    # stack the input dataset
+    stacked = ds.stack(allpoints = ("lat", "lon"))
 
-    for lat in range(len(bins[0])):
-        for lon in range(len(bins[0][0])):
-            y = ds.isel(lat = lat, lon = lon).groupby_bins(ds[var].isel(lat = lat, lon = lon), bins[:, lat, lon]).quantile(percentile_val, interpolation='midpoint')
+    # loop through the dataset to get binned values and then unstack
+    for point in range(len(bins[1])):
+        y = stacked.isel(allpoints = point).groupby_bins(stacked[var].isel(allpoints = point), bins[:, point]).quantile(percentile_val, interpolation='midpoint')
+    unstacked = y.unstack("allpoints")
 
-            bin_array = y.coords[var_bin].to_numpy()
+    # get the slope using the bin array and precip
+    bin_array = unstacked.coords[var_bin].to_numpy()
+    precip = unstacked.precipitationCal.to_numpy()
+    slope = np.apply_along_axis(get_slope, 0, bin_array, precip)
 
-            bin_mids = []
-
-            for i in range(0, len(bin_array)):
-                bin_mid = (bin_array[i].left + bin_array[i].right)*0.5
-                bin_mids.append(bin_mid)
-
-            mids = np.array(bin_mids)
-
-            if np.isnan(np.sum(y.precipitationCal.to_numpy())):
-                slope = np.nan
-            else:
-                slope = np.polyfit(mids, y.precipitationCal.to_numpy(), 1)[0]
-
-            binned_ds[lat, lon] = binned_ds[lat, lon] + slope
-
-    ccscale = xr.DataArray(binned_ds, dims=("lat", "lon"), coords={"lat": ds.coords['lat'], "lon": ds.coords['lon']}, attrs=dict(description="C-C scale", units="degC$^{-1}$"))
-
+    # make the ccscale data array and return
+    ccscale = xr.DataArray(slope, dims=("lat", "lon"), coords={"lat": ds.coords['lat'], "lon": ds.coords['lon']}, attrs=dict(description="C-C scale", units="degC$^{-1}$"))
     return ccscale
--- util/gpmdown.py
+++ util/gpmdown.py
@@ -1,40 +1,52 @@
 from typing import Sequence
 import requests
 from multiprocessing.pool import ThreadPool
-from requests.sessions import Session, session
+# from requests.sessions import Session, session
 from os.path import exists
 
-username = "adisen99"
-password = "Sai@baba99"
+from requests.api import head
+
+headers = {
+    'Accept-Encoding': 'gzip',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.06',
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'Keep-Alive',
+}
 
 def download_url(url):
+  # assumes that the last segment after the / represents the file name
+  # if url is abc/xyz/file.txt, the file name will be file.txt
     file_name_start_pos = url.rfind("/") + 1
-    file_name_end_pos = url.rfind("?")
-    filename = url[file_name_start_pos:file_name_end_pos]
+    file_name_end_pose = url.find("?")
+    file_name = url[file_name_start_pos:file_name_end_pose]
 
-    if not exists(filename):
-        print("downloading: ", url)
-        with requests.Session() as session:
-            session.auth = (username, password)
-            r1 = session.request('get', url)
-            r = session.get(r1.url, auth=(username, password))
-            if r.ok:
-                with open(filename, 'wb') as f:
-                    for data in r:
-                        f.write(data)
+    if not exists(file_name):
+        print("downloading: ",url)
+        r = requests.get(url, stream=True, headers=headers)
+        if r.status_code == requests.codes.ok:
+          with open(file_name, 'wb') as f:
+            for data in r:
+              f.write(data)
+        return url
 
-# Get the urls from the txt file as a list using readline
-file1 = open('links.txt', 'r')
-Lines = file1.readlines()
+def main(link_list):
+    # Get the urls from the txt file as a list using readline
+    file1 = open(link_list, 'r')
+    Lines = file1.readlines()
 
-urls = []
-count = 0
-# Strips the newline character
-for line in Lines:
-    count += 1
-    urls.append(line.strip())
+    urls = []
+    count = 0
+    # Strips the newline character
+    for line in Lines:
+        count += 1
+        urls.append(line.strip())
 
-# Run 5 multiple threads. Each call will take the next element in urls list
-results = ThreadPool(6).imap_unordered(download_url, urls)
-for r in results:
-    print("Completed" + r)
+    # Run 5 multiple threads. Each call will take the next element in urls list
+    results = ThreadPool(4).imap_unordered(download_url, urls)
+    for r in results:
+        print(r)
+
+if __name__ == "__main__":
+    main()
