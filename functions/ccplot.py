104501f6f6412a6bf5a1671c288427121ea07679
adisen99 <adityasenguptasai99@gmail.com>
2021-11-10 20:28:49 +0530

    lab: added and trying combining script

--- functions/ccplot.py
+++ functions/ccplot.py
@@ -5,14 +5,15 @@ from scipy import stats
 
 # Preparation for the ideal C-C scaling background plots
 # TODO - implement another background plot of 2*CC scaling or 1.5*CC scaling
-def get_ideal_data(da, p11_inital_val, p12_initial_value):
+def get_ideal_data(da, p11_inital_val, p12_initial_value, time_scale=1):
     temparr = np.sort(da)
     p11 = [p11_inital_val]
     p12 = [p12_initial_value]
 
     for i in range(0, len(temparr) - 1):
-        p21 = p11[i] * (1.068**(temparr[i + 1] - temparr[i]))
-        p22 = p12[i] * (1.068**(temparr[i + 1] - temparr[i]))
+        # C-C scaling
+        p21 = p11[i] * ((1 + time_scale*0.068)**(temparr[i + 1] - temparr[i]))
+        p22 = p12[i] * ((1 + time_scale*0.068)**(temparr[i + 1] - temparr[i]))
         p11.append(p21)
         p12.append(p22)
 
@@ -22,7 +23,7 @@ def get_ideal_data(da, p11_inital_val, p12_initial_value):
     return temparr, preciparr1, preciparr2
 
 # The main plot function
-def plot(ds, binned_ds, bin_var, temparr, preciparr1, preciparr2, fit = True, **kwargs):
+def plot(ds, binned_ds, bin_var, temparr, preciparr1, preciparr2, preciparr3, preciparr4, fit = True, **kwargs):
     # get the mid points of the temperature bins
     bin_mids = []
     bin_array = binned_ds.coords[bin_var].to_numpy()
@@ -42,9 +43,11 @@ def plot(ds, binned_ds, bin_var, temparr, preciparr1, preciparr2, fit = True, **
         # binned_ds.precipitationCal.plot(**kwargs)
         plt.semilogy(temparr, preciparr1, 'k--', alpha = 0.3)
         plt.semilogy(temparr, preciparr2, 'k--', alpha = 0.3)
+        plt.semilogy(temparr, preciparr3, 'k:', alpha = 0.3)
+        plt.semilogy(temparr, preciparr4, 'k:', alpha = 0.3)
         plt.plot(mids, slope*mids + intercept, color = 'k', ls = '-', alpha = 0.8, label = f'C-C scale = {np.round(100*(np.exp(slope) - 1), 3)}; $R^2$ = {np.round(r,3)}')
-        plt.xlim(mids.min() - 1, mids.max() + 1)
-        plt.yticks([1, 10, 100])
+        plt.xlim(mids.min() - 0.2, mids.max() + 0.2)
+        # plt.yticks([1, 10, 100])
         plt.legend(frameon = False)
 
     elif fit == False:
@@ -54,6 +57,8 @@ def plot(ds, binned_ds, bin_var, temparr, preciparr1, preciparr2, fit = True, **
         # binned_ds.precipitationCal.plot(**kwargs)
         plt.semilogy(temparr, preciparr1, 'k--', alpha = 0.3)
         plt.semilogy(temparr, preciparr2, 'k--', alpha = 0.3)
-        plt.xlim(mids.min() - 1, mids.max() + 1)
-        plt.yticks([1, 10, 100])
+        plt.semilogy(temparr, preciparr3, 'k:', alpha = 0.3)
+        plt.semilogy(temparr, preciparr4, 'k:', alpha = 0.3)
+        plt.xlim(mids.min() - 0.2, mids.max() + 0.2)
+        # plt.yticks([1, 10, 100])
         plt.legend(frameon = False)
---
+++ util/combine_data.py
@@ -0,0 +1,63 @@
+# Python script to combine temperature and precipitation data for binning
+
+import numpy as np
+import xarray as xr
+import dask
+from dask.diagnostics import ProgressBar
+import time
+
+# the main function to run
+def main():
+    print("Importing GPM data ...")
+    mfdata_DIR = '../data/test/gpm*.nc'
+    gpm = xr.open_mfdataset(mfdata_DIR, chunks=dict(time=10000, lat=40, lon=40), engine='netcdf4', combine='nested', concat_dim='time', parallel=True)
+    gpm.unify_chunks()
+    print("done ...")
+    time.sleep(1)
+
+    print("Now resampling precip data ...")
+    precip = gpm.precipCal.resample(time='1H').mean()
+    precip = precip.chunk(dict(time=10000, lat=40, lon=40))
+    precip.unify_chunks()
+    print("done resampling ...")
+    time.sleep(1)
+    
+    print("Importing era data ...")
+    mfdata_DIR2 = '../data/test/era*.nc'
+    era = xr.open_mfdataset(mfdata_DIR2, chunks=dict(time=10000, lat=40, lon=40), engine='netcdf4', combine='nested', concat_dim='time', parallel=True)
+    with dask.config.set(**{'array.slicing.split_large_chunks':False}):
+        era = era.reindex(latitude = era.latitude[::-1])
+
+    era = era.sel(time = slice("2000-06-01 00:00:00","2021-06-30 23:00:00"))
+    era.unify_chunks()
+    # era = era.sel(expver=1, drop=True)
+    era = era.transpose('time', 'latitude', 'longitude')
+    era = era.rename_dims({'longitude':'lon', 'latitude':'lat'})
+    era = era.rename({'longitude':'lon', 'latitude':'lat'})
+    print("done ...")
+    time.sleep(1)
+
+    print("Now extracting T2 and D2 ...")
+    t2m = era.t2m
+    d2m = era.d2m
+    t2m = t2m.chunk(dict(time=10000, lat=40, lon=40))
+    d2m = d2m.chunk(dict(time=10000, lat = 40, lon=40))
+    print("done ...")
+    time.sleep(1)
+
+    print("Now combining into new dataset ...")
+    ds_comb = xr.merge([precip, t2m, d2m])
+    print("done ...")
+    time.sleep(1)
+
+    print("Now exporting to disk ...")
+    delayedObj = ds_comb.to_netcdf('../data/combined/ds_comb2.nc', compute=False)
+
+    with ProgressBar():
+        results = delayedObj.compute()
+
+    print(results)
+
+if __name__ == "__main__":
+    main()
+    print("Finished")
