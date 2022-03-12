0ce60cba5156779815b152b4513a164a993905f1
adisen99 <adityasenguptasai99@gmail.com>
2022-03-12 11:37:24 +0530

    fix: fixing the era5 parallel download script

--- util/eradown_parallel.py
+++ util/eradown_parallel.py
@@ -1,40 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov  1 12:54:04 2021
-
-@author: picard
-"""
+# New parallel download script
 
 import cdsapi
-import concurrent.futures
+import multiprocessing
 
-def download(yr):
-    print(yr)
-    c = cdsapi.Client()
-    c.retrieve(
-    'reanalysis-era5-pressure-levels',
-    {
+client = cdsapi.Client()
+
+def cdsapi_worker(dataset):
+    result = client.retrieve('reanalysis-era5-pressure-levels', dataset)
+    result.download(dataset['file_name'])
+
+def single_dataset(yr, file_prefix):
+    return {
         'product_type': 'reanalysis',
         'format': 'netcdf',
-        'variable': 'u_component_of_wind',
-        'pressure_level': '850',
-        'year': [
-            '1982', '1983', '1984',
-            '1985', '1986', '1987',
-            '1988', '1989', '1990',
-            '1991', '1992', '1993',
-            '1994', '1995', '1996',
-            '1997', '1998', '1999',
-            '2000', '2001', '2002',
-            '2003', '2004', '2005',
-            '2006', '2007', '2008',
-            '2009', '2010', '2011',
-            '2012', '2013', '2014',
-            '2015', '2016', '2017',
-            '2018', '2019', '2020',
-            '2021',
+        'variable': ['u_component_of_wind', 'v_component_of_wind'],
+        'area': [
+            40, 60, 0,
+            100,
         ],
+        'pressure_level': ['200', '850'],
+        'year': str(yr),
         'month': [
             '01', '02', '03',
             '04', '05', '06',
@@ -55,18 +40,25 @@ def download(yr):
             '31',
         ],
         'time': [
-            '00:00', '03:00', '06:00',
-            '09:00', '12:00', '15:00',
-            '18:00', '21:00',
+            '00:00', '01:00', '02:00',
+            '03:00', '04:00', '05:00',
+            '06:00', '07:00', '08:00',
+            '09:00', '10:00', '11:00',
+            '12:00', '13:00', '14:00',
+            '15:00', '16:00', '17:00',
+            '18:00', '19:00', '20:00',
+            '21:00', '22:00', '23:00',
         ],
-        'area': [
-            35, 40, -5,
-            110,
-        ],
-    },
-    'U850/u850'+str(yr)+'.nc')
-    return None
+        # making an arbitrary entry to the dict
+        'file_name': file_prefix+'_'+str(yr)+'.nc'
+    }
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
-    years = range(1982,2022)
-    results = executor.map(download, years)
+# make the changes to this file to get the necessary values parallely
+datasets = [single_dataset(yr, 'wind_pres') for yr in range(2001,2021)]
+
+# https://cds.climate.copernicus.eu/live/queue
+# per-user requests that access online CDS data is 3 at maximum
+
+# so we overbook by one with 4 processes
+pool = multiprocessing.Pool(processes=4)
+pool.map(cdsapi_worker, datasets)
